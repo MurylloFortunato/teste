7b7cc41cceea78ee50ed23c9dc395dd83230f00b
MurylloFortunato <122032695+MurylloFortunato@users.noreply.github.com>
2023-07-31 16:50:08 -0300

    Add files via upload

---
+++ Fastech2/produtos.js
@@ -0,0 +1,75 @@
+let produtosJson = [
+  {
+    id: 1,
+    name: 'Caixinha de som Kapbom ',
+    img: 'images/fasttech1 (1).png',
+    price: [20.00, 23.00, 200.00],
+    sizes: [
+      '$5',
+      'R$7',
+      'R$4'
+    ],
+    description: 'Além de entradas usb, tf, o modelo possui sinal de rádio fm. As caixas de som bluetooth da kapbom tem um som incrível ,elas são compactas, leves ,ótimas para carregar com você para qualquer lugar.'
+  },
+  {
+    id: 2,
+    name: 'Fone bluetooth H’maston preto',
+    img: 'images/fasttech1 (2).png',
+    price: [21.00, 24.00, 26.00],
+    sizes: [
+      'R$5',
+      'R$7',
+      'R$4'
+    ],
+    description: 'Acompanha bluetooth na versão 5.0 garantindo uma alta transmissão de áudio, além de vir com a tecnologia TWS que também favorece na qualidade do produto. Sua bateria é de longa duração assim como a sua vida útil.'
+  },
+  {
+    id: 3,
+    name: 'SmartWatch Lucky Lin ',
+    img: 'images/fasttech1 (3).png',
+    price: [23.00, 26.00, 250.00],
+    sizes: [
+      '$5',
+      'R$7',
+      'R$4',
+    ],
+    description: 'Acompanha bluetooth na versão 5.0 garantindo uma alta transmissão de áudio, além de vir com a tecnologia TWS que também favorece na qualidade do produto. Sua bateria é de longa duração assim como a sua vida útil.'
+  
+  },
+  {
+    id: 4,
+    name: 'Relógio Haylou  ',
+    img: 'images/fasttech1 (4).png',
+    price: [270.00, 265.00, 250.00],
+    sizes: [
+      '$20',
+      'R$15',
+      'R$--',
+    ],
+    description: '1,69" tela sensível ao toque para homens e mulheres, SpO2, fmonitor de frequência cardíaca, IP68 Relógio esportivo, Relógio inteligente para Android iOS'
+    },
+  {
+    id: 5,
+    name: 'Feijoada dos Vianna',
+    img: 'images/fasttech1 (5).png',
+    price: [25.00, 28.00, 30.00],
+    sizes: [
+      '$5',
+      'R$7',
+      'R$4',
+    ],
+    description: 'Acompanha bluetooth na versão 5.0 garantindo uma alta transmissão de áudio, além de vir com a tecnologia TWS que também favorece na qualidade do produto. Sua bateria é de longa duração assim como a sua vida útil.'
+  },
+  {
+    id: 6,
+    name: 'Pizza de Doritos',
+    img: 'images/fasttech1 (6).png',
+    price: [25.00, 28.00, 30.00],
+    sizes: [
+      '$5',
+      'R$7',
+      'R$4',
+    ],
+    description: '1,69" tela sensível ao toque para homens e mulheres, SpO2, fmonitor de frequência cardíaca, IP68 Relógio esportivo, Relógio inteligente para Android iOS'
+  },
+];
